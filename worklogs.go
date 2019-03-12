96dc14383cd670f7c465a7bb4abe0bbc857dbc6a
Leo Adamek <leo.adamek@mrzen.com>
2019-03-12 12:18:07 +0000

    Temporarily remove billable seconds

--- worklogs.go
+++ worklogs.go
@@ -26,25 +26,25 @@ func (c *Client) CreateWorklog(ctx context.Context, worklog *Worklog) error {
 // MarshalJSON marshals the Worklog into JSON
 func (w Worklog) MarshalJSON() ([]byte, error) {
 	timeSpentSeconds := int64(math.Ceil(w.TimeSpent.Seconds()))
-	billableTimeSeconds := int64(math.Ceil(w.BillableTime.Seconds()))
+	// billableTimeSeconds := int64(math.Ceil(w.BillableTime.Seconds()))
 	startDate := w.Start.Format("2006-02-01")
 	startTime := w.Start.Format("15:04:03")
 
 	return json.Marshal(&struct {
 		IssueKey         string `json:"issueKey"`
 		TimeSpentSeconds int64  `json:"timeSpentSeconds"`
-		BillableSeconds  int64  `json:"billableSeconds"`
-		StartDate        string `json:"startDate"`
-		StartTime        string `json:"startTime"`
-		AuthorAccountID  string `json:"authorAccountId"`
-		Description      string `json:"description"`
+		// BillableSeconds  *int64 `json:"billableSeconds,omitempty"`
+		StartDate       string `json:"startDate"`
+		StartTime       string `json:"startTime"`
+		AuthorAccountID string `json:"authorAccountId"`
+		Description     string `json:"description"`
 	}{
 		IssueKey:         w.IssueKey,
 		TimeSpentSeconds: timeSpentSeconds,
-		BillableSeconds:  billableTimeSeconds,
-		StartDate:        startDate,
-		StartTime:        startTime,
-		AuthorAccountID:  w.UserID,
-		Description:      w.Description,
+		// BillableSeconds:  billableTimeSeconds,
+		StartDate:       startDate,
+		StartTime:       startTime,
+		AuthorAccountID: w.UserID,
+		Description:     w.Description,
 	})
 }
